7e4d48e7e351b3c2d19aecd2126acb2ac00f5fcc
Corey Schmierer <s120786@coreys-mbp.auiag.corp>
2020-03-04 14:51:43 +1100

    Initial Commit

---
+++ genAndDeal.py
@@ -0,0 +1,32 @@
+import random
+
+def genCardPack():
+    cardPack = []
+    suits = ['H','S','C','D']
+    cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K']
+    for suit in suits:
+        for card in cards:
+            cardPack.append(card+suit)
+    return cardPack
+
+def shuffleCardPack(cardPack):
+    for i in range(1000000):
+        cardSelection = random.randint(0, 51)
+        cardPack[0], cardPack[cardSelection] = cardPack[cardSelection], cardPack[0]
+    return cardPack
+
+def formPlayerHands(playerCount):
+    playerHands = []
+    for i in range(playerCount):
+        playerHands.append(['',''])
+    return playerHands
+
+def dealP1(cardPack, playerCount):
+    playerHands = formPlayerHands(playerCount)
+    for player in range(playerCount):
+        for i in range(2):
+            playerHands[player][i] = cardPack.pop()
+    return playerHands, cardPack
+
+def genAndDeal(playerCount):
+    cardPack = 
