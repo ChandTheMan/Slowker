00e7f3ca1b1055a3cb35457180a142d3e46cbe5d
Corey Schmierer <s120786@coreys-mbp.auiag.corp>
2020-03-05 15:03:40 +1100

    Moved to object fun times

--- slowker/checks.py
+++ slowker/checks.py
@@ -1,5 +1,5 @@
 """Deck/Hand based checks to perform."""
-
+'''
 
 def face_card_convert(card):
     """Convert from a deck card A->K into it's numerical representation."""
@@ -17,6 +17,7 @@ def face_card_convert(card):
         elif card == 'K':
             return 13
 
+'''
 
 def high_card_check(hand, suit_to_check: str):
     """Get the highest card in a given hand.
@@ -25,9 +26,9 @@ def high_card_check(hand, suit_to_check: str):
     """
     if suit_to_check:
         for i in range(len(hand)-1, -1, -1):
-            if hand[i][1] == suit_to_check:
-                return hand[i]
-    return hand[-1]
+            if hand[i].suit == suit_to_check:
+                return hand[i].icon
+    return hand[-1].icon
 
 
 def pair_check(hand):
@@ -36,11 +37,11 @@ def pair_check(hand):
     pair_list = []
     for i in range(len(hand)-1):
         try:
-            if hand[i][0] == hand[i+1][0] and hand[i+1][0] != pair_list[-1]:
-                pair_list.append(hand[i][0])
+            if hand[i].value == hand[i+1].value and hand[i+1].value != pair_list[-1]:
+                pair_list.append(hand[i].value)
         except IndexError:
-            if hand[i][0] == hand[i+1][0]:
-                pair_list.append(hand[i][0])
+            if hand[i].value == hand[i+1].value:
+                pair_list.append(hand[i].value)
     if len(pair_list) == 0:
         return False
     if len(pair_list) <= 2:
@@ -53,11 +54,11 @@ def trips_check(hand):
     tripsList = []
     for i in range(len(hand)-2):
         try:
-            if hand[i][0] == hand[i+1][0] == hand[i+2][0] and hand[i+1][0] != tripsList[-1]:
-                tripsList.append(hand[i][0])
+            if hand[i].value == hand[i+1].value == hand[i+2].value and hand[i+1].value != tripsList[-1]:
+                tripsList.append(hand[i].value)
         except IndexError:
-            if hand[i][0] == hand[i+1][0] == hand[i+2][0]:
-                tripsList.append(hand[i][0])
+            if hand[i].value == hand[i+1].value == hand[i+2].value:
+                tripsList.append(hand[i].value)
     if len(tripsList) == 0:
         return False
     return tripsList[-1]
@@ -65,19 +66,20 @@ def trips_check(hand):
 
 def straight_check(hand):
     """Find straights in a given hand."""
-    straight_list = [face_card_convert(hand[0][0])]
+    straight_list = [hand[0].raw_value]
     
     for i in range(1, len(hand)):
-        if face_card_convert(hand[i][0]) == straight_list[-1]:
+
+        if hand[i].raw_value == straight_list[-1]:
             continue
-        elif face_card_convert(hand[i][0]) != straight_list[-1]+1 and len(straight_list) < 5:
-            straight_list = [face_card_convert(hand[i][0])]
-        elif face_card_convert(hand[i][0]) == straight_list[-1]+1 and len(straight_list) == 5:
+        elif hand[i].raw_value != straight_list[-1]+1 and len(straight_list) < 5:
+            straight_list = [hand[i].raw_value]
+        elif hand[i].raw_value == straight_list[-1]+1 and len(straight_list) == 5:
             straight_list.pop(0)
-            straight_list.append(face_card_convert(hand[i][0]))
-        elif face_card_convert(hand[i][0]) == straight_list[-1]+1:
-            straight_list.append(face_card_convert(hand[i][0]))
-        elif face_card_convert(hand[i][0]) != straight_list[-1]+1 and len(straight_list) == 5:
+            straight_list.append(hand[i].raw_value)
+        elif hand[i].raw_value == straight_list[-1]+1:
+            straight_list.append(hand[i].raw_value)
+        elif hand[i].raw_value != straight_list[-1]+1 and len(straight_list) == 5:
             return straight_list
 
     if len(straight_list) < 5:
@@ -90,19 +92,19 @@ def flush_check(hand):
     """Find flush in a given hand."""
     suit_count = [0, 0, 0, 0]
     for i in hand:
-        if i[1] == 'C':
+        if i.suit == 'C':
             suit_count[0] += 1
             if suit_count[0] == 5:
                 return high_card_check(hand, 'C')
-        elif i[1] == 'D':
+        elif i.suit == 'D':
             suit_count[1] += 1
             if suit_count[1] == 5:
                 return high_card_check(hand, 'D')
-        elif i[1] == 'H':
+        elif i.suit == 'H':
             suit_count[2] += 1
             if suit_count[2] == 5:
                 return high_card_check(hand, 'H')
-        elif i[1] == 'S':
+        elif i.suit == 'S':
             suit_count[3] += 1
             if suit_count[3] == 5:
                 return high_card_check(hand, 'S')
--- slowker/slowker.py
+++ slowker/slowker.py
@@ -1,23 +1,25 @@
 # from slack import WebClient
-
 from gen_and_deal import game
+from gen_and_deal import card_pack
 from gen_and_deal import player
+from gen_and_deal import card
+
 
 player_1 = player("Corey", 500)
 player_2 = player("Algy", 420)
+
 
 def main():
     """Slowker is slow poker!"""
     gamey = game()
     gamey.add_player(player_1) #Temp solution to add players
     gamey.add_player(player_2)
-    gamey.gen_card_pack()
-    gamey.shuffle_card_pack()
     gamey.form_player_hands()
     gamey.deal_flop()
     gamey.deal_turn()
     gamey.deal_river()
-    print(gamey.player_list[0].hand)
+    gamey.update_all_hands()
+    print(gamey.player_list[1].hand_value)
     #hand_compare.hand_compare(player_hands, board)
 
     return
