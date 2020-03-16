43b9c2cf7700d4779fb24aecca7cb596f83dc918
Corey Schmierer <corey.schmierer@iag.com.au>
2020-03-16 18:54:22 +1100

    Bet round logic implemented
    corey commented out break
    
    corey commented out break
    
    Implemented betting functionality

--- slowker/player.py
+++ slowker/player.py
@@ -4,9 +4,11 @@ class player:
     def __init__(self, name, bank_roll):
         self.name = name
         self.bank_roll = bank_roll
+        self.play_status = True #True for in play, false for folded
         self.hand = []
         self.hand_worth = []    #Empty container for later hand worth rank
         self.hand_value = []    #Empty container for hand value storage from check function
+        self.self_pot = 0
         return
 
     def update_total_hand(self, board):     #Combines board and hand and keeps it in player data
@@ -40,6 +42,45 @@ class player:
         self.hand_sort_number()
         self.hand_value = hand_compute(self.total_hand)
         return
+    
+    def bet(self, bet_amount):
+        assert bet_amount <= self.bank_roll, "Bet is higher than bank roll"
+        self.bank_roll -= bet_amount-self.self_pot
+        final_amount = bet_amount-self.self_pot
+        self.self_pot = bet_amount
+        return [final_amount, self.play_status, 'bet', bet_amount]
+    
+    def call(self, amount):
+        if self.bank_roll < amount-self.self_pot:
+            amount = self.bank_roll
+            self.bank_roll = 0
+            self.self_pot += amount
+            return [amount, self.play_status, 'call']
+        else:
+           
+            self.bank_roll -= amount-self.self_pot
+            final_amount = amount-self.self_pot
+            self.self_pot += amount-self.self_pot
+        
+            return [final_amount, self.play_status, 'call']
+    
+    def fold(self):
+        self.play_status = False
+        return [0, self.play_status, 'fold']
+
+    def player_action_call(self, action, current_bet):
+        while True:
+            if action == 'bet':
+                return self.bet(int(input("How much to bet?: ")))
+            if action == 'call':
+                return self.call(current_bet)
+            if action == 'fold':
+                return self.fold()
+            if action == 'check cards':
+                print("Your hand is: ", end = "") 
+                print(self.hand[0].icon, self.hand[1].icon)
+            action = input("What would you like to do now?: ")
+
 
     def print_icon(self):       #A method to print a players cards, don't use regular print!
         for i in range(len(self.total_hand)):
--- slowker/slowker.py
+++ slowker/slowker.py
@@ -3,25 +3,34 @@
 
 from player import player
 from poker_game import poker_game
+from bet_round import bet_round
 
 player_1 = player("Corey", 500) #Create player, with name, bankroll
 player_2 = player("Algy", 420)
+player_3 = player("Michael \'Mr. Finternational\' Finn", 100)
+player_4 = player("Rex", 5000)
 
 
 def main():
     """Slowker is slow poker!"""
-    game = poker_game()
+    game = poker_game(20)
     game.add_player(player_1)   #Temp solution to add players to game
     game.add_player(player_2)
+    game.add_player(player_3)
+    game.add_player(player_4)
     game.form_player_hands()    #Deal phase one, creating card objects and card_pack  
     game.update_all_hands()     #Used after each round, to sort hand + calculate each persons strongest hand
+    game.first_bet()
     game.deal_flop()
     game.update_all_hands()
+    #game.standard_bet_round()
     game.deal_turn()
     game.update_all_hands()
+    #game.standard_bet_round()
     game.deal_river()
     game.update_all_hands()
-    print(game.player_list[1].hand_value)   #Prints hand_value of player at index 1
+    #game.standard_bet_round()
+    #print(game.player_list[1].hand_value)   #Prints hand_value of player at index 1
     #hand_compare.hand_compare(player_hands, board) --Incomplete
     return
 
